e27181c02f9699d88b81533c9c7ff835919e13e8
Carlos Blanco <carlojbm04@gmail.com>
2024-06-30 13:55:37 +0200

    Subdomains

---
+++ README.md
@@ -0,0 +1,14 @@
+# Hacking with Python
+
+## Details
+
+> python --version 3.11.7
+
+### Package List
+
+```py
+    # Information recognition
+    pip install dnspython
+    pip install argparse
+    pip install requests
+```
---
+++ subdomains/subdomain.py
@@ -0,0 +1,37 @@
+import requests
+from os import path
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--target', help='Detect Subdomain')
+parser = parser.parse_args()
+
+def URL(url: str) -> None:
+    try:
+        requests.get(url)
+    except requests.exceptions.RequestException as e:
+        print(f'(-) Not Found: {url}')
+    else:
+        print(f'(+) Subdomains: {url}')
+
+def main():
+
+    if parser.target:
+        if path.exists('subdomain.txt'):
+            data = open('./subdomain.txt', 'r')
+            data = data.read().split('\n')
+
+            for subdomain in data:
+                URL('http://'+subdomain+'.'+parser.target)
+                URL('https://'+subdomain+'.'+parser.target)
+
+    else:
+        print('Err: Please provide a valid subdomain')
+        sys.exit()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
